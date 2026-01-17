dbe580bbfac377efc9d1f2cc747a921ff8bf0673
friedlore <leeyeeun358@gmail.com>
2026-01-17 18:47:23 +0900

    fix : 게임 씬 UI 픽스

---
+++ Assets/Scripts/GameManager/UICanvasManager.cs
@@ -0,0 +1,18 @@
+using System.Threading;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UICanvasManager : MonoBehaviour
+{
+    public Image myGameTimer;
+    public TextMeshProUGUI myGameScore;
+
+
+    void OnEnable()
+    {
+        GameSceneManager.Instance.gameTimer = myGameTimer;
+        GameSceneManager.Instance.gameScore = myGameScore;
+
+    }
+}
