322ff10dfe8c69f56c777ee71fb515717ccb43ed
lee-ye-eun <leeyeeun358@gmail.com>
2026-04-12 01:13:34 +0900

    feat: 로비 커튼 시스템 적용

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,22 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class LobbyManager : MonoBehaviour
 {
     [SerializeField] private LobbyPanel lobbyPanel; // 로비 패널 참조
+    [SerializeField] private Image curtainImage; // 커튼 이미지 참조
+    [SerializeField] private Vector2 curtainImagePosition = new Vector2(0, 0f); // 커튼 이미지 위치
 
     [Header("Scene Names")]
     [SerializeField] private string GameSceneName = "MainScene";
 
+    private Sequence _seq;
+
     public void Start()
     {
+        _seq?.Kill();
+
         GameManager.Instance.soundManager.PlayBGM(SoundManager.BGM.Title);
+
+        curtainImage.rectTransform.anchoredPosition = new Vector2(0, 0); // 커튼 이미지 초기 위치 설정
+        curtainImagePosition = new Vector2(0, 0); // 커튼 이미지 적용할 포지션 초기화
+
+        for (int i = 0; i < GameManager.Instance.GameData.stageUnlocked.Count; i++)
+        {
+            if (i != 0 && GameManager.Instance.GameData.stageUnlocked[i])
+            {
+                curtainImagePosition += new Vector2(400f, 0); // 커튼 이미지 적용할 포지션
+            }
+            else if (i != 0)
+            {
+                break;
+            }
+        }
+        _seq.Join(curtainImage.rectTransform.DOAnchorPos(curtainImagePosition, 2f).SetEase(Ease.OutExpo));
     }
     public void onStageButton(int stageIndex)
     {
-        GameManager.Instance.soundManager.PlaySFX(SoundManager.SFX.ButtonClick);
-
         if (GameManager.Instance.GameData.stageUnlocked[stageIndex])
         {
+            GameManager.Instance.soundManager.PlaySFX(SoundManager.SFX.ButtonClick);
             lobbyPanel.openStageSelectPanel(stageIndex);
         }
         else
