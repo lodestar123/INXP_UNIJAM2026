d6e6dcb8125b3ea99cb1f011f744605b64e3f28f
lee-ye-eun <leeyeeun358@gmail.com>
2026-05-17 15:48:00 +0900

    feat: 컷씬 다시보기 씬 생성
    - 컷씬 다시보기 씬 생성
    - 각 버튼 누르면 볼 수 있는 컷씬만 재생

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -10,6 +10,7 @@ public class LobbyManager : MonoBehaviour
 
     [Header("Scene Names")]
     [SerializeField] private string GameSceneName = "MainScene";
+    [SerializeField] private string ReViewSceneName = "ReViewScene";
 
     private Sequence _seq;
 
@@ -48,5 +49,11 @@ public class LobbyManager : MonoBehaviour
             Debug.Log("해금되지 않은 스테이지입니다.");
         }
 
+    }
+
+    public void onReViewButton()
+    {
+        GameManager.Instance.soundManager.PlaySFX(SoundManager.SFX.ButtonClick);
+        SceneLoader.Load(ReViewSceneName);
     }
 }
