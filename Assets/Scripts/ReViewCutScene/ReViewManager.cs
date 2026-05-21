f56fd815ceaadeba2640d4496f2947c57870e0d3
lee-ye-eun <leeyeeun358@gmail.com>
2026-05-22 02:37:31 +0900

    fix: 스프라이트 수정, 다시보기 수정
    - 다시보기 버튼의 아이콘 스프라이트 할당 로직을 생성 및 수정
    - 설정, 다시보기 아이콘 스프라이트 수정

--- Assets/Scripts/ReViewCutScene/ReViewManager.cs
+++ Assets/Scripts/ReViewCutScene/ReViewManager.cs
@@ -10,31 +10,69 @@ public class ReViewManager : MonoBehaviour
     [SerializeField] private string CutSceneName = "CutScene";
     [SerializeField] private string ReViewSceneName = "ReViewScene";
 
-    //[SerializeField] private Image cutSceneImage; // 컷씬 이미지 참조
+    [Header("Cutscene Buttons")]
+    [SerializeField] private Button[] cutSceneButtons;   // 버튼 6개
+
+    [Header("Button Sprites")]
+    [SerializeField] private Sprite lockedSprite; // 잠긴 스테이지 버튼 이미지
+    [SerializeField] private Sprite unlockedSprite; // 해금된 스테이지 버튼 이미지
 
     void Start()
     {
         GameManager.Instance.soundManager.PlayBGM(SoundManager.BGM.Title);
         settigPanel.SetActive(false); // 설정 패널 초기화 - 숨김
 
-        // 이미지 초기화 - 각 스테이지 컷씬 해금 여부에 따라 이미지 변경
-        // cutSceneImage.sprite = null;
+        InitializeCutSceneButtons();
+    }
+    private void InitializeCutSceneButtons()
+    {
+        for (int i = 0; i < cutSceneButtons.Length; i++)
+        {
+            bool isUnlocked = IsButtonUnlocked(i);
+
+            Image btnImage = cutSceneButtons[i].GetComponent<Image>();
+            btnImage.sprite = isUnlocked ? unlockedSprite : lockedSprite;
+        }
+    }
+
+    private bool IsButtonUnlocked(int buttonIndex)
+    {
+        var gameData = GameManager.Instance.GameData;
+
+        switch (buttonIndex)
+        {
+            case 0:
+                return true; // 인트로
+
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                // 이전 스테이지 하이스코어가 클리어 기준 이상이면 언락
+                int scoreIndex = buttonIndex - 1;
+                return gameData.stageHighScore[scoreIndex] >= gameData.stageClearCriteria[scoreIndex];
+
+            case 5:
+                // 막스테 클리어시 자동 언락
+                return IsButtonUnlocked(4);
+
+            default:
+                return false;
+        }
     }
 
     public void onGoToCutScene(int stageIndex)
     {
         GameManager.Instance.soundManager.PlaySFX(SoundManager.SFX.ButtonClick);
 
-        if (GameManager.Instance.GameData.stageUnlocked[stageIndex + 1])
+        if (IsButtonUnlocked(stageIndex + 1))
         {
-            GameManager.Instance.currentStageNum = stageIndex; // 컷씬에서 보여줄 스테이지 번호 지정
-            GameManager.Instance.nextSceneAfterCutscene = ReViewSceneName; // 컷씬 이후 다시보기 씬으로 이동해야 함 지정
-
+            GameManager.Instance.currentStageNum = stageIndex;
+            GameManager.Instance.nextSceneAfterCutscene = ReViewSceneName;
             SceneLoader.Load(CutSceneName);
         }
         else
         {
-
             Debug.Log("해금되지 않은 스테이지입니다.");
         }
     }
